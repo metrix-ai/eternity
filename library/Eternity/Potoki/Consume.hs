aaa02f0c425341e8c5e79d6f03a68866ac16696b
vlastachu <vlastachu@gmail.com>
2018-08-23 13:35:49 +0300

    add flag for flush on Consume

--- library/Eternity/Potoki/Consume.hs
+++ library/Eternity/Potoki/Consume.hs
@@ -1,10 +1,9 @@
-module Eternity.Potoki.Consume
-where
+module Eternity.Potoki.Consume where
 
-import Eternity.Prelude hiding (fold)
-import Potoki.Consume
-import qualified Eternity.IO.FileNames as D
-import qualified Potoki.Cereal.Transform as E
+import           Eternity.Prelude        hiding ( fold )
+import           Potoki.Consume
+import qualified Eternity.IO.FileNames         as D
+import qualified Potoki.Cereal.Transform       as E
 
 
 {-|
@@ -12,17 +11,28 @@ Write the events in a file in the specified directory, overwriting it if it alre
 
 If any file IO error appears, Consume returns it wrapped in Left.
 -}
-writeToFile :: Serialize event => FilePath -> Consume event (Either IOException ())
-writeToFile =
-  transform E.encode . writeBytesToFile
+writeToFile
+  :: Serialize event
+  => Bool -- ^ on `True` stream will be flushed on each event, on `False` buffering will be enabled
+  -> FilePath
+  -> Consume event (Either IOException ())
+writeToFile flushOnEachEvent =
+  let consumer = if flushOnEachEvent then writeBytesToFileWithoutBuffering else writeBytesToFile
+  in  transform E.encode . consumer
 
 {-|
 Write the events to a new file in the specified directory.
 
 If any file IO error appears, Consume returns it wrapped in Left.
 -}
-writeToDir :: Serialize event => FilePath -> Consume event (Either IOException ())
-writeToDir path =
-  liftIO (D.determineNextEventFilePath path) >>=
-  traverse (transform E.encode . writeBytesToFile) >>=
-  return . join
+writeToDir
+  :: Serialize event
+  => Bool -- ^ on `True` stream will be flushed on each event, on `False` buffering will be enabled
+  -> FilePath
+  -> Consume event (Either IOException ())
+writeToDir flushOnEachEvent path =
+  let consumer = if flushOnEachEvent then writeBytesToFileWithoutBuffering else writeBytesToFile
+  in  liftIO (D.determineNextEventFilePath path)
+      >>= traverse (transform E.encode . consumer)
+      >>= return
+      .   join
