c8cbe60a08c02aa3355ded38510ae57a39d2b5c2
vlastachu <vlastachu@gmail.com>
2018-09-18 14:54:16 +0300

    fix tests

--- test/Main.hs
+++ test/Main.hs
@@ -19,18 +19,25 @@ main =
   testGroup "Store and read events" $
   [
     testProperty "Store and read events" $ \ (events :: [Int]) ->
-    unsafePerformIO $ do
-      removePathForcibly directory
-      createDirectoryIfMissing True directory
-      writeEvents events
-      readEventsResult <- readEvents
-      return (Right (Right events) === readEventsResult)
+      unsafePerformIO $ do
+        removePathForcibly directory
+        createDirectoryIfMissing True directory
+        writeEvents False events
+        readEventsResult <- readEvents
+        return (Right (Right events) === readEventsResult)
+  , testProperty "Store and read events with flushing" $ \ (events :: [Int]) ->
+      unsafePerformIO $ do
+        removePathForcibly directory
+        createDirectoryIfMissing True directory
+        writeEvents True events
+        readEventsResult <- readEvents
+        return (Right (Right events) === readEventsResult)
   ]
   where
     directory =
       "dist/test-events"
-    writeEvents events =
-      G.produceAndConsume (F.list events) (P.writeToDir directory)
+    writeEvents flush events =
+      G.produceAndConsume (F.list events) (P.writeToDir flush directory)
     readEvents =
       G.produceAndConsume
         (O.readFromDir directory)
